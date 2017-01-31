86054b0d758e546e3a35d344b388d4d9ec6de2e6
Kirill Timukov <k_timukov@mail.ru>
2017-01-31 19:37:55 +0300

    Добавлен ajax, изменены представления, доработка верстки

--- task_1/urls.py
+++ task_1/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'discription', views.example_result, name='example_result'),
     url(r'^', views.main, name='main'),
 ]
--- task_1/views.py
+++ task_1/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from task_1.models import Examples
+from django.http import HttpResponse
 
 
 def main(request):
     return render(request, 'main.html', {'Examples': Examples.objects.all()})
+
+
+def example_result(request):
+    e_name = request.GET.get('name')
+    discription = Examples.objects.filter(name=e_name)
+    res = discription[0].example
+    return HttpResponse(res)
